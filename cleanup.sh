8b850001ac02020df983a8f9da25386d28ce8900
Jim <jim@arnellconsulting.com>
2017-12-24 09:54:02 +0100

    Updating project cleanly

---
+++ cleanup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+rm -rf .angular-cli.json
+rm -rf .editorconfig
+rm -rf .gitattributes
+rm -rf .gitignore
+rm -rf .idea
+rm -rf .jhipster
+rm -rf LICENSE.md
+rm -rf .mvn
+rm -rf mvnw
+rm -rf mvnw.cmd
+rm -rf node_modules
+rm -rf package.json
+rm -rf pom.xml
+rm -rf proxy.conf.json
+rm -rf README.md
+rm -rf src
+rm -rf target
+rm -rf .travis.yml
+rm -rf tsconfig-aot.json
+rm -rf tsconfig.json
+rm -rf tslint.json
+rm -rf webpack
+rm -rf worktajm.iml
+rm -rf yarn.lock
+
+jhipster --force
+jhipster import-jdl jdl.jdl --force
+./mvnw
