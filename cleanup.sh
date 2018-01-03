25b8d19ecd89055f5cd75b878c8ed2dcf4def9bb
Jim <jim@arnellconsulting.com>
2018-01-03 12:00:37 +0100

    Update cleanup script.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,20 @@
 #!/bin/zsh
 
-rm -rf ^(cleanup.sh|LICENSE.md|.jhipster|.yo-rc.json|.git|.jdl)
+rm -rf .angular-cli.json
+rm -rf .idea
+rm -rf .mvn
+rm -rf mvnw
+rm -rf mvnw.cmd
+rm -rf package.json
+rm -rf pom.xml
+rm -rf postcss.config.js
+rm -rf proxy.conf.json
+rm -rf src
+rm -rf tsconfig-aot.json
+rm -rf tsconfig.json
+rm -rf tslint.json
+rm -rf webpack
+rm -rf yarn.lock
 
 if [ -d ".jhipster" ]
 then
@@ -12,7 +26,7 @@ fi
 
 if grep -q angular ".yo-rc.json"; then
     yarn add @angular/material hammerjs
-    yarn add  @angular/cdk
+    yarn add @angular/cdk
     yarn add @angular/animations
 fi
 
