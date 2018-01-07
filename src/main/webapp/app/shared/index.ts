8adc7b3444a3775b9ed80a1722a4350428dadfa1
Jim <jim@arnellconsulting.com>
2018-01-07 20:10:50 +0100

    .

--- src/main/webapp/app/shared/index.ts
+++ src/main/webapp/app/shared/index.ts
@@ -27,4 +27,3 @@ export * from './social/social.component';
 export * from './shared-libs.module';
 export * from './shared-common.module';
 export * from './shared.module';
-export * from './worktajm/duration-format.pipe';
